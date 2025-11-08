d3c5cf48cb557125d195d2f5ff7b20ef7d066a05
Matt <66947952+xenoperdix@users.noreply.github.com>
2025-11-08 20:18:48 +0000

    refactor: use createPagesBrowserClient

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,9 +1,9 @@
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs";
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs";
 import type { SupabaseClient } from "@supabase/supabase-js";
 import type { Database } from "../types";
 
 export const createClientSupabaseClient = (): SupabaseClient<Database> =>
-  createBrowserSupabaseClient<Database>(
+  createPagesBrowserClient<Database>(
     {
       supabaseUrl: process.env.NEXT_PUBLIC_SUPABASE_URL!,
       supabaseKey: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
